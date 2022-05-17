51afcdb741dbadbfc161833d6655801a43cb712b
iktefish <87042742+iktefish@users.noreply.github.com>
2022-05-18 00:33:54 +0600

    Update environment

--- shell.nix
+++ shell.nix
@@ -9,9 +9,25 @@ in
 stdenv.mkDerivation {
   name = "rust";
   buildInputs = with pkgsUnstable; [
+    gcc
+    mesa
+    xorg.libX11
+    xorg.libXrandr
+    xorg.libXcursor
+    xorg.libXinerama
+    xorg.libXi
+    xorg.libXxf86vm
+    xorg.libXext # for Xge.h
+    xorg.libxcb
+    libGL
+    libGL.dev
+    alsa-lib
+    pkg-config
+
     rustc
     cargo
     rust-analyzer
     rustfmt
+    clippy
   ];
 }
