60004a72163a558f792cce56716a60a86fd9413b
iktefish <87042742+iktefish@users.noreply.github.com>
2022-05-19 09:34:24 +0600

    Add Player structure

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,41 @@ enum GameMode {
     End,
 }
 
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+    fn constructor(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'))
+    }
+
+    fn gravity_and_mode(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap(&mut self) {
+        self.velocity += -0.2;
+    }
+}
+
 struct State {
     mode: GameMode,
 }
