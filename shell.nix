0954c6d5dda57db100085ba1e12fb0842e734c16
iktefish <87042742+iktefish@users.noreply.github.com>
2022-09-12 18:24:59 +0600

    Changes to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,33 +1,20 @@
-with import <nixpkgs> { };
-let
-  unstableTarball =
-    fetchTarball
-      https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
-  pkgsUnstable = import unstableTarball { };
-
-in
-stdenv.mkDerivation {
-  name = "rust";
-  buildInputs = with pkgsUnstable; [
-    gcc
-    mesa
-    xorg.libX11
-    xorg.libXrandr
-    xorg.libXcursor
-    xorg.libXinerama
-    xorg.libXi
-    xorg.libXxf86vm
-    xorg.libXext # for Xge.h
-    xorg.libxcb
-    libGL
-    libGL.dev
-    alsa-lib
-    pkg-config
-
-    rustc
-    cargo
-    rust-analyzer
-    rustfmt
-    clippy
-  ];
-}
+with import <nixpkgs> {};
+  stdenv.mkDerivation {
+    name = "rust";
+    buildInputs = with pkgs; [
+      gcc
+      mesa
+      xorg.libX11
+      xorg.libXrandr
+      xorg.libXcursor
+      xorg.libXinerama
+      xorg.libXi
+      xorg.libXxf86vm
+      xorg.libXext # for Xge.h
+      xorg.libxcb
+      libGL
+      libGL.dev
+      alsa-lib
+      pkg-config
+    ];
+  }
